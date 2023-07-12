5354a9b533025a16375c53ca0d09426ba9b4aa27
felix <felix.spoerri@ervocom.ch>
2023-07-12 16:16:58 +0200

    bongo bongo

--- src/chat/SettingsSection.jsx
+++ src/chat/SettingsSection.jsx
@@ -1,54 +1,37 @@
 import React from 'react'
+import {SenderList} from "../components/useAi";
 
 export function SettingsSection({
-                           tempAiName,
-                           aiName,
-                           tempAttributes,
-                           attributes,
-                           onChangeAiName,
-                           onChangeAttributes,
-                           tmpApiKey,
-                           apiKey,
-                           onChangeApiKey,
-                           saveSettings,
-                           resetConversation
-                         }) {
-  function toHidden(tmpApiKey) {
-    if (tmpApiKey.length > 6 && tmpApiKey === apiKey) {
-      // get first 5 characters of api key the rest is hidden with •
-      return tmpApiKey.substring(0, 6) + '•'.repeat(tmpApiKey.length - 6)
-    }
-  }
+                                    aiPersonality,
+                                    setPersonality,
+                                    apiKey,
+                                    setApiKey,
+                                    resetConversation
+                                }) {
+    return <section id="aside" className="full md:half lg:quarter lg:screen-v-scroll flex row wrap">
+        <div className="pt px">
+            <div className={'orange'}>Personality</div>
+            <select
+                value={aiPersonality}
+                onChange={(e) => setPersonality(e.target.value)}>
+                {
+                    SenderList.map((item, index) => (
+                        <option key={index} value={item}>{item}</option>
+                    ))
+                }
+            </select>
 
-  return <section id="aside" className="full md:half lg:quarter lg:screen-v-scroll flex row wrap">
-    <div className="pt px">
-      <form onSubmit={saveSettings}>
-        <span className={tempAiName !== aiName ? 'fuschia' : 'orange'}>AI Name</span>
-        <input type="text"
-               spellCheck={false}
-               value={tempAiName}
-               placeholder={'AI Name'}
-               onChange={onChangeAiName}/>
-        <br/>
-        <br/>
-        <span className={tempAttributes !== attributes ? 'fuschia' : 'orange'}>Attributes</span>
-        <input type="text"
-               spellCheck={false}
-               value={tempAttributes}
-               placeholder={'Attributes'}
-               onChange={onChangeAttributes}/>
-        <br/>
-        <br/>
-        <span className={tmpApiKey !== apiKey || tmpApiKey === '' ? 'fuschia' : 'orange'}>API Key</span>
-        <input type="text"
-               spellCheck={false}
-               value={toHidden(tmpApiKey)}
-               placeholder={'API Key'}
-               onChange={onChangeApiKey}/>
-        <button type="submit" style={{visibility: 'hidden'}}>Save</button>
-      </form>
-      <a href={'#'} onClick={saveSettings} className={'green hover'}>Save</a>
-      <a href={'#'} onClick={resetConversation} className={'fuschia hover'}>Reset</a>
-    </div>
-  </section>
+            <br/>
+            <br/>
+            <span className={'orange'}>API Key</span>
+            <input type="text"
+                   spellCheck={false}
+                   value={apiKey}
+                   placeholder={'API Key'}
+                   onChange={(e) => setApiKey(e.target.value)}/>
+            <br/>
+            <br/>
+            <a href={'#'} onClick={resetConversation} className={'fuschia hover'}>Reset</a>
+        </div>
+    </section>
 }
